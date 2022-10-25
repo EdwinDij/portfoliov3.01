cf216a7c7f96c068bc9cd51ff9792d561246ab34
EdwinDij <edwin.d899@gmail.com>
2022-10-25 12:34:37 +0200

    navbar

---
+++ pages/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <nav className='flex font-bold justify-between'> 
+        <div>
+            <h1>ED</h1>
+        </div>
+        <div className='flex gap-10'>
+           <Link href="/">
+            <a className='hover:text-indigo-500'>Home</a>
+           </Link>
+           <Link href="/project">
+            <a className='hover:text-indigo-500'>Projets</a>
+           </Link>
+           <Link href="/About">
+            <a className='hover:text-indigo-500'>À propos</a>
+           </Link>
+           <Link href="/Contact">
+            <a className='hover:text-indigo-500'>Contact</a>
+           </Link>
+        </div>
+    </nav>
+  )
+}
